cf30833b3478325824346c22fa920acb48b3b269
Serghei Ivanov <serghei.ivanov@slyce.it>
2018-03-20 12:12:59 -0400

    added json-format.directive and json-formatter-js npm module

---
+++ src/app/directives/json-format.directive.ts
@@ -0,0 +1,35 @@
+import { Directive, Renderer2, ElementRef, OnChanges, Input } from '@angular/core';
+import JSONFormatter from 'json-formatter-js';
+
+@Directive({
+  selector: '[appJsonFormat]'
+})
+export class JsonFormatDirective implements OnChanges {
+
+  @Input() json;
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnChanges() {
+    const options = {
+      hoverPreviewEnabled: true,
+      hoverPreviewArrayCount: 100,
+      hoverPreviewFieldCount: 2,
+    };
+
+    console.log(this.json);
+
+
+    if (typeof this.json === 'string') {
+      this.json = JSON.parse(this.json);
+    }
+
+    const formatter = new JSONFormatter(this.json, 1, options);
+    const elem = formatter.render();
+    this.renderer.appendChild(this.el.nativeElement, elem);
+  }
+
+}
