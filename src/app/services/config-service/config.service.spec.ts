124d2bbc7bae94ab584f45ea05a0f33238945d4b
knt36 <kntran10@gmail.com>
2018-03-22 11:14:41 -0400

    some basic unit test

--- src/app/services/config-service/config.service.spec.ts
+++ src/app/services/config-service/config.service.spec.ts
@@ -1,11 +1,24 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ConfigService } from './config.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('ConfigService', () => {
+  let configService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ConfigService]
+      providers: [ConfigService],
+      imports: [HttpClientModule]
+    });
+    configService = TestBed.get(ConfigService);
+
+  });
+  it('should be created', function () {
+    expect(configService).toBeTruthy();
+  });
+  it('should load config file', function () {
+    configService.initConfigService().then(res => {
+      expect(res).toBeTruthy();
     });
   });
 });
