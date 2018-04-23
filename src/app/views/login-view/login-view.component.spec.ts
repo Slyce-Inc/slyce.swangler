05078620c1b15c426ba73d9af04141c1d5cda03b
Serghei Ivanov <serghei.ivanov@slyce.it>
2018-04-23 17:49:54 -0400

    added login component tests

--- src/app/views/login-view/login-view.component.spec.ts
+++ src/app/views/login-view/login-view.component.spec.ts
@@ -1,14 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { LoginViewComponent } from './login-view.component';
+import { Observable } from 'rxjs/Observable';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-describe('LoginViewComponent', () => {
+const storage = {
+  username: 'test_name',
+  password: 'test_password'
+};
+const LocalStorageServiceStub = {
+  getStorageVar: (varName) => {
+    return storage ? storage[varName] : null;
+  },
+  setStorageVar: (varName, varVal) => {
+    storage[varName] = varVal;
+  },
+  setStorageSecurityDef: (varName, varVal) => {
+    storage[varName] = varVal;
+  },
+  ready: new BehaviorSubject(true)
+};
+
+fdescribe('LoginViewComponent', () => {
   let component: LoginViewComponent;
   let fixture: ComponentFixture<LoginViewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginViewComponent ]
+      declarations: [
+        LoginViewComponent
+      ],
+      imports: [
+        FormsModule,
+      ],
+      providers: [
+        { provide: LocalStorageService, useValue: LocalStorageServiceStub },
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +49,12 @@ describe('LoginViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set initial credentials', () => {
+    component.ngOnInit();
+
+    expect(component.username).toEqual('test_name');
+    expect(component.password).toEqual('test_password');
   });
 });
