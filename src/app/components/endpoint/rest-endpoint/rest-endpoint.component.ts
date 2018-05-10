6bd41f9278861593cd7cef88026548670d045521
knt36 <kntran10@gmail.com>
2018-05-10 14:52:14 -0400

    Fixexd cannot sub on empty object or custom objects on endpoints

--- src/app/components/endpoint/rest-endpoint/rest-endpoint.component.ts
+++ src/app/components/endpoint/rest-endpoint/rest-endpoint.component.ts
@@ -85,6 +85,9 @@ export class RestEndpointComponent extends EndpointComponent implements OnInit {
               // apply to just that field
               // I got to go to this path.....
               pathArray.reduce((prevVal, curVal) => {
+                if (!prevVal[curVal]) {
+                  prevVal[curVal] = {};
+                }
                 if (curVal === lastPathItem) {
                   prevVal[curVal] = this.altInputs[path];
                 }
