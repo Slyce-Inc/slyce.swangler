e5beb2b41dea3644810e98e7a38e2f9404a64f81
knt36 <kntran10@gmail.com>
2018-05-11 16:40:29 -0400

    Applied the fix, it was not looping through tags before going through the endpoints

--- src/app/services/shared-vars.service.ts
+++ src/app/services/shared-vars.service.ts
@@ -14,38 +14,42 @@ export class SharedVarsService implements OnInit {
   ngOnInit() {
   }
 
-  initSharedVars(endpoints) {
+  initSharedVars(tags) {
     const res = {};
-    endpoints.forEach(endpoint => {
-      if ( endpoint.parameters && endpoint.parameters.length ) {
-        endpoint.parameters.forEach(param => {
-          if (!param.default && !res[param.name]) {
-            let sharedVarName;
-            if (param.in === 'body') {
-              sharedVarName = endpoint.operationId + '_body';
-            } else {
-              sharedVarName = param.name;
-            }
-            res[sharedVarName] = new BehaviorSubject(null);
-            const localStorageVal = this.localStorageService.getStorageVar(sharedVarName);
-            if ( localStorageVal ) {
-              res[sharedVarName].next(localStorageVal);
-            }
+    if (tags) {
+      Object.keys(tags).forEach( tag => {
+        tags[tag].forEach(endpoint => {
+          if ( endpoint.parameters && endpoint.parameters.length ) {
+            endpoint.parameters.forEach(param => {
+              if (!param.default && !res[param.name]) {
+                let sharedVarName;
+                if (param.in === 'body') {
+                  sharedVarName = endpoint.operationId + '_body';
+                } else {
+                  sharedVarName = param.name;
+                }
+                res[sharedVarName] = new BehaviorSubject(null);
+                console.log(sharedVarName);
+                const localStorageVal = this.localStorageService.getStorageVar(sharedVarName);
+                if ( localStorageVal ) {
+                  res[sharedVarName].next(localStorageVal);
+                }
+              }
+            });
+          }
+          if (endpoint.requestMessages) {
+            endpoint.requestMessages.forEach((requestMessage, i) => {
+              const sharedVarName = endpoint.operationId + '_ws_message_' + i;
+              res[sharedVarName] = new BehaviorSubject(null);
+              const localStorageVal = this.localStorageService.getStorageVar(sharedVarName);
+              if ( localStorageVal ) {
+                res[sharedVarName].next(localStorageVal);
+              }
+            });
           }
         });
-      }
-      if (endpoint.requestMessages) {
-        endpoint.requestMessages.forEach((requestMessage, i) => {
-          const sharedVarName = endpoint.operationId + '_ws_message_' + i;
-          res[sharedVarName] = new BehaviorSubject(null);
-          const localStorageVal = this.localStorageService.getStorageVar(sharedVarName);
-            if ( localStorageVal ) {
-              res[sharedVarName].next(localStorageVal);
-            }
-        });
-      }
-    });
-
+      });
+    }
     this.sharedVars = res;
   }
   setSharedVars(key: string, value) {
